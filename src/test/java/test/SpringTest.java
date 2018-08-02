4b5ce2a397e558e83bb972698cb3a4397575b145
zhao.xiaohu <m.zhaox@foxmail.com>
2018-08-02 15:54:32 +0800

    添加测试

---
+++ src/test/java/test/SpringTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+
+import com.ep.entity.Sysuser;
+import com.ep.service.UserInfoService;
+
+/**
+* @author  Zhaoxh
+* @version 创建时间：2018年8月2日 下午3:31:14
+* 测试类
+*/
+public class SpringTest extends BaseSpringTest {
+	
+	@Autowired
+    public UserInfoService userInfoService;
+
+	@Test
+	public void selectAllUser() {
+		List<Sysuser> users = userInfoService.findAll();
+		for(Sysuser user : users) {
+			System.out.println("UserName:"+user.getUserName() + "\n" + "PassWord:" + user.getPassword());
+		}
+	}
+	
+	@Transactional
+	@Rollback(true)
+	@Test
+	public void insert() {
+		String i = userInfoService.addUser("userName5", "123456", "", "1");
+		System.out.println(i);
+		
+	}
+}
